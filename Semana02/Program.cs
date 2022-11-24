d8368a32200779e4846996f8597dc8eeb442732e
Anderson Almeyda <51338520+COMANDO225@users.noreply.github.com>
2022-11-24 15:14:12 -0500

    Agregar archivos de proyecto.

---
+++ Semana02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.Read();
+            int edad = 34;
+            string nombre = "Anderson";
+            decimal sueldo = 10000;
+            bool estado = true;
+        }
+    }
+}
