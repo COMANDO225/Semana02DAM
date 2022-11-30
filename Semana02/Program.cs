7336518b35ad2a01b39c287269e51f80645a7432
Anderson Almeyda <51338520+COMANDO225@users.noreply.github.com>
2022-11-29 19:20:23 -0500

    Listo

--- Semana02/Program.cs
+++ Semana02/Program.cs
@@ -10,12 +10,47 @@ namespace Semana02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+
+            Console.Write("Ingrese coordenada x1: ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese coordenada y1: ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("punto A: (" +x1+ ","+y1+")");
+            Console.WriteLine(" ");
+
+            Console.Write("Ingrese coordenada x2: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese coordenada y2: ");
+            double y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("punto B: (" + x2 + "," + y2 + ")");
+            Console.WriteLine(" ");
+
+            Console.Write("Ingrese coordenada x3: ");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese coordenada y3: ");
+            double y3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("punto C: (" + x3 + "," + y3 + ")");
+            Console.WriteLine(" ");
+
+            Console.Write("Ingrese coordenada x4: ");
+            double x4 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese coordenada y4: ");
+            double y4 = double.Parse(Console.ReadLine());
+            Console.WriteLine("punto D: (" + x4 + "," + y4 + ")");
+            Console.WriteLine(" ");
+
+            Rectangulo rectangulo = new Rectangulo();
+            rectangulo.x1 = x1;
+            rectangulo.y1 = y1;
+            rectangulo.x2 = x2;
+            rectangulo.y2 = y2;
+            rectangulo.x3 = x3;
+            rectangulo.y3 = y3;
+            rectangulo.x4 = x4;
+            rectangulo.y4 = y4;
+
+            Console.WriteLine("eL area del rectangulo es: " + rectangulo.Area() + " mi King");
             Console.Read();
-            int edad = 34;
-            string nombre = "Anderson";
-            decimal sueldo = 10000;
-            bool estado = true;
         }
     }
 }
---
+++ Semana02/Rectangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02
+{
+    public class Rectangulo
+    {
+        // punto A
+        public double x1 { get; set; }
+        public double y1 { get; set; }
+        // punto B
+        public double x2 { get; set; }
+        public double y2 { get; set; }
+        // punto C
+        public double x3 { get; set; }
+        public double y3 { get; set; }
+        // punto D
+        public double x4 { get; set; }
+        public double y4 { get; set; }
+
+        public double Area()
+        {
+            double side1 = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            double side2 = Math.Sqrt(Math.Pow(x2 - x3, 2) + Math.Pow(y2 - y3, 2));
+
+            return side1 * side2;
+        }
+    }
+}
